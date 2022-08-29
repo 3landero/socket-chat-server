e620aa0311dde698c4183a8e85c1a99174a494a3
Ramses Landero <27landero@gmail.com>
2022-08-29 01:35:06 -0500

    OK PORT

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const {Server} = require('socket.io')
 
 app.use(cors())
 
+const PORT = process.env.PORT || 3000;
 
 const server= http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ['GET','POST']
     }
 })
@@ -37,7 +38,7 @@ io.on("connection", (socket)=>{
     })
 })
 
-server.listen(3001, ()=>{
+server.listen(PORT, ()=>{
     console.log('SERVER RUNNING');
 });
 
